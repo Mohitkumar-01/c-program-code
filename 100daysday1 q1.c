a022a70be8a40877bd1be900ce08973a9335334d
Mohitkumar-01 <mohittkumar.in@gmail.com>
2025-08-31 21:47:21 +0530

    Add files via upload
    100 days of code day question1

---
+++ 100daysday1 q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x,y,sum;
+    printf("enter first number: ");
+    scanf("%d",&x);
+    printf("enter second number: ");
+    scanf("%d",&y);
+    
+    sum = x + y;
+    printf("sum = %d\n", sum);
+    return 0;
+}
+
