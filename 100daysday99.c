d1103654bf753bbba6ec48182c51ae1af50f3473
Mohitkumar-01 <mohittkumar.in@gmail.com>
2025-12-11 17:13:19 +0530

    100daysday99.c

---
+++ 100daysday99.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+
+int main() {
+    struct Student *s;
+
+    s = (struct Student *)malloc(sizeof(struct Student));
+
+    if (s == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    scanf("%s %d %d", s->name, &s->roll, &s->marks);
+
+    printf("Name: %s | Roll: %d | Marks: %d", s->name, s->roll, s->marks);
+
+    free(s);
+
+    return 0;
+}
