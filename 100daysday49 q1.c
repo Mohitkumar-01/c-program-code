fdc666ff80b067921c6c9e71f146e1d5c90b3d10
Mohitkumar-01 <mohittkumar.in@gmail.com>
2025-12-11 16:43:20 +0530

    100daysday49 q1.c

---
+++ 100daysday49 q1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    
+    char sentence[200];
+    int i, start, end;
+    
+    printf("Enter a sentence: ");
+    gets(sentence);
+
+    i = 0;
+    while (sentence[i] != '\0') {
+        while (sentence[i] == ' ' && sentence[i] != '\0') i++;
+        start = i;
+        while (sentence[i] != ' ' && sentence[i] != '\0') i++;
+        end = i - 1;
+
+        while (start < end) {
+            char temp = sentence[start];
+            sentence[start] = sentence[end];
+            sentence[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    printf("Output: %s\n", sentence);
+    return 0;
+}
