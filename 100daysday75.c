4ecb1fda499043a91cfa8446fdda9a0f615ffb87
Mohitkumar-01 <mohittkumar.in@gmail.com>
2025-12-11 16:59:13 +0530

    100daysday75.c

---
+++ 100daysday75.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char filename[100];
+    char text[200];
+
+    printf("Enter the filename: ");
+    scanf("%s", filename);
+    getchar(); 
+
+    fp = fopen(filename, "a");
+    if (fp == NULL) {
+        printf("Error! Could not open file.\n");
+        return 1;
+    }
+
+    printf("Enter text to append: ");
+    fgets(text, sizeof(text), stdin);
+
+    fputs("\n", fp);        
+    fputs(text, fp);
+
+    
+    fclose(fp);
+
+    printf("File updated successfully with appended text.\n");
+
+    return 0;
+}
