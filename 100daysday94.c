5fb4c1e63122a9cfee68161e56a8bde33d58fac0
Mohitkumar-01 <mohittkumar.in@gmail.com>
2025-12-11 17:10:45 +0530

    100daysday94.c

---
+++ 100daysday94.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    int marks;
+};
+
+void printStudent(struct Student s) {
+    printf("Name: %s | Roll: %d | Marks: %d\n", s.name, s.roll_no, s.marks);
+}
+
+int main() {
+    struct Student s;
+
+    
+    scanf("%s %d %d", s.name, &s.roll_no, &s.marks);
+
+    // Call function to print details
+    printStudent(s);
+
+    return 0;
+}
