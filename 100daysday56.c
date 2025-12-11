d9e23dc970a5456713088ad31b672667a7b4ca82
Mohitkumar-01 <mohittkumar.in@gmail.com>
2025-12-11 16:49:11 +0530

    100daysday56.c

---
+++ 100daysday56.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter array elements: ");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(int i = 0; i < n; i++){
+        int nextGreater = -1;  
+        for(int j = i + 1; j < n; j++){
+            if(arr[j] > arr[i]){
+                nextGreater = arr[j];
+                break; 
+            }
+        }
+        printf("%d", nextGreater);
+        if(i != n - 1)
+            printf(", ");
+    }
+
+    return 0;
+}
