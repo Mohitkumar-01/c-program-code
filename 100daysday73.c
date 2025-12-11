40c61ba263bc67eea370ef8032749349c4f36162
Mohitkumar-01 <mohittkumar.in@gmail.com>
2025-12-11 16:58:17 +0530

    100daysday73.c

---
+++ 100daysday73.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char ch;
+    int characters = 0, words = 0, lines = 0;
+    int inWord = 0;  
+
+    fp = fopen("sample.txt", "r");
+
+    if (fp == NULL) {
+        printf("Error! File not found.\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        characters++;
+
+        if (ch == '\n')
+            lines++;
+
+        if (ch == ' ' || ch == '\n' || ch == '\t') {
+            inWord = 0;
+        } else if (inWord == 0) {
+            inWord = 1;
+            words++;
+        }
+    }
+
+    fclose(fp);
+
+    if (characters > 0 && ch != '\n')
+        lines++;
+
+    printf("Characters: %d\n", characters);
+    printf("Words: %d\n", words);
+    printf("Lines: %d\n", lines);
+
+    return 0;
+}
