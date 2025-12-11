24ace8582cd57fa039192f969ce906c4386ab9f9
Mohitkumar-01 <mohittkumar.in@gmail.com>
2025-12-11 17:07:12 +0530

    v

---
+++ 100daysday90.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+};
+
+struct Person {
+    enum Gender gender;
+};
+
+int main() {
+    char input[20];
+    struct Person p;
+
+
+    scanf("%[^=]=%s", input, input);   
+
+    if (strcmp(input, "MALE") == 0)
+        p.gender = MALE;
+    else if (strcmp(input, "FEMALE") == 0)
+        p.gender = FEMALE;
+    else
+        p.gender = OTHER;
+
+    // Print gender in proper format
+    switch (p.ge
