abc8f26c5d4d17a14a4e1bf2489bee111b3e0790
Mohitkumar-01 <mohittkumar.in@gmail.com>
2025-12-11 17:05:14 +0530

    100daysday86.c

---
+++ 100daysday86.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+enum Operation {
+    ADD,
+    SUBTRACT,
+    MULTIPLY
+};
+
+int main() {
+    char opStr[10];
+    int a, b;
+    enum Operation op;
+
+    // Example input format: ADD 10 20
+    scanf("%s %d %d", opStr, &a, &b);
+
+    // Map string to enum
+    if (strcmp(opStr, "ADD") == 0)
+        op = ADD;
+    else if (strcmp(opStr, "SUBTRACT") == 0)
+        op = SUBTRACT;
+    else if (strcmp(opStr, "MULTIPLY") == 0)
+        op = MULTIPLY;
+    else
+        return 0;   
+
+    switch (op) {
+        case ADD:
+            printf("%d\n", a + b);
+            break;
+        case SUBTRACT:
+            printf("%d\n", a - b);
+            break;
+        case MULTIPLY:
+            printf("%d\n", a * b);
+            break;
+    }
+
+    return 0;
+}
