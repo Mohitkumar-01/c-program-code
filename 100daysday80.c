2825bf3f5a05d0ab6239c89dfc8baf89acedf990
Mohitkumar-01 <mohittkumar.in@gmail.com>
2025-12-11 17:01:53 +0530

    100daysday80.c

---
+++ 100daysday80.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    int n, roll, marks;
+    char name[50];
+    
+    fp = fopen("students.txt", "w");
+    if (fp == NULL) {
+        printf("Error opening file for writing!\n");
+        return 1;
+    }
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter details for student %d:\n", i + 1);
+        printf("Name: ");
+        scanf("%s", name);
+        printf("Roll Number: ");
+        scanf("%d", &roll);
+        printf("Marks: ");
+        scanf("%d", &marks);
+
+        fprintf(fp, "%s %d %d\n", name, roll, marks);
+    }
+    fclose(fp);
+
+    fp = fopen("students.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file for reading!\n");
+        return 1;
+    }
+
+    printf("\n--- Student Records ---\n");
+    while (fscanf(fp, "%s %d %d", name, &roll, &marks) == 3) {
+        printf("Name: %s | Roll: %d | Marks: %d\n", name, roll, marks);
+    }
+
+    fclose(fp);
+    return 0;
+}
